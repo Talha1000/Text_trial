47a7122febb27e2827a6dca490b4e57746cbba25
Talha1000 <talhaewu@gmail.com>
2024-01-16 13:29:08 +0600

    Insert Data

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import mysql.connector
+from mysql.connector import errorcode
+
+i = 0
+
+
+def insert_data_from_text_file(filename, cursor, connection):
+    with open(filename, 'r') as file:
+        global i
+        for line in file:
+            data = line.strip().split(' | ')
+            if len(data) != 6:
+                print(
+                    f"Error: Expected 6 values, but got {len(data)} values in line: {line}")
+                continue
+            else:
+                i += 1
+                print([i], data[2], data[3] + "\t[Data Recorded]")
+
+            query = "insert into people(user_id, first_name, last_name, gender, email, dob) values (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(query, tuple(data))
+
 
 conn = mysql.connector.connect(
     user='root',
@@ -9,11 +30,10 @@ conn = mysql.connector.connect(
 
 mycursor = conn.cursor()
 
-mycursor.execute("SELECT * FROM people")
-
-myresult = mycursor.fetchall()
-
-for x in myresult:
-    print(x)
+insert_data_from_text_file('peoples.txt', mycursor, conn)
 
 conn.commit()
+print("\nData Record Successful.")
+
+mycursor.close()
+conn.close()
