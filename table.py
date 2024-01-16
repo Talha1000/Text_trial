69df28493674815f6cb600a41879662a77f99269
Talha1000 <talhaewu@gmail.com>
2024-01-16 13:28:33 +0600

    Create Table

--- table.py
+++ table.py
@@ -1,17 +1,18 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-db = mysql.connector.connect(
+conn = mysql.connector.connect(
     user='root',
     password='',
     host='localhost',
     database='trial_db'
 )
 
-mycursor = db.cursor()
+mycursor = conn.cursor()
 
 mycursor.execute(
-    "create table people (Serial int auto_increment primary key, NID varchar(255), first_name varchar(255), last_name varchar(255), gender varchar(255), email varchar(255), Date_of_Birth date)"
+    "create table people (id int auto_increment primary key, user_id varchar(255), first_name varchar(255), last_name varchar(255), gender varchar(255), email varchar(255), dob date)"
 )
 
-db.commit()
+conn.commit()
+print("Table Created.")
